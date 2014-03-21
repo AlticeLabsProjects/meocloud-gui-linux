1fb28ec9d74089002b84aff06184de180fe421db
Ivo Nunes <ivo-a-nunes@telecom.pt>
2014-03-21 17:45:20 +0000

    added some utils to meocloud-nautilus

--- meocloud_nautilus/meocloud-nautilus.py
+++ meocloud_nautilus/meocloud-nautilus.py
@@ -5,6 +5,13 @@ class UpdateFileInfoAsync(Nautilus.InfoProvider, GObject.GObject):
     def __init__(self):
         pass
 
+    def get_local_path(self, path):
+        return path.replace("file://", "")
+
+    def valid_uri(self, uri):
+        if not uri.startswith("file://"): return False
+        return True
+
     def update_file_info(self, item):
         # test
         if "MEOCloud" in item.get_uri():
