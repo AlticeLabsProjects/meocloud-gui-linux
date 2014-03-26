63125c4e24d4228a4c768632f1e1496067ed0ddb
Ivo Nunes <ivo-a-nunes@telecom.pt>
2014-03-26 15:20:08 +0000

    more features for the dolphin plugin. almost on par with nautilus.

--- meocloud_dolphin/dolphin-meocloud-plugin.cpp
+++ meocloud_dolphin/dolphin-meocloud-plugin.cpp
@@ -19,44 +19,139 @@
 K_PLUGIN_FACTORY(DolphinMEOCloudPluginFactory, registerPlugin<DolphinMEOCloudPlugin>();)
 K_EXPORT_PLUGIN(DolphinMEOCloudPluginFactory("dolphin-meocloud-plugin"))
 
-struct DolphinMEOCloudPlugin::Private
-{
-    QString contextFilePath;
-};
-
-
 DolphinMEOCloudPlugin::DolphinMEOCloudPlugin(QObject* parent, const QVariantList & args):
-    KAbstractFileItemActionPlugin(parent)
+    KVersionControlPlugin(parent),
+    m_versionInfoHash()
 {
     Q_UNUSED(args);
-    p = new Private;
 
-    // connect to core
-    // p->core
+    m_shareFolderAction = new KAction(this);
+    m_shareFolderAction->setIcon(KIcon("internet-web-browser"));
+    m_shareFolderAction->setText(i18nc("@item:inmenu", "Share Folder"));
+    connect(m_shareFolderAction, SIGNAL(triggered()),
+            this, SLOT(shareFolderAction()));
+
+    m_openInBrowserAction = new KAction(this);
+    m_openInBrowserAction->setIcon(KIcon("internet-web-browser"));
+    m_openInBrowserAction->setText(i18nc("@item:inmenu", "Open in Browser"));
+    connect(m_openInBrowserAction, SIGNAL(triggered()),
+            this, SLOT(openInBrowserAction()));
+
+    m_shareFileLinkAction = new KAction(this);
+    m_shareFileLinkAction->setIcon(KIcon("internet-web-browser"));
+    m_shareFileLinkAction->setText(i18nc("@item:inmenu", "Copy Link"));
+    connect(m_shareFileLinkAction, SIGNAL(triggered()),
+            this, SLOT(shareFileLinkAction()));
 }
 
 DolphinMEOCloudPlugin::~DolphinMEOCloudPlugin()
 {
-    delete p;
 }
 
-QList<QAction*> DolphinMEOCloudPlugin::actions(const KFileItemListProperties & fileItemInfos, QWidget * parentWidget)
+QString DolphinMEOCloudPlugin::fileName() const
 {
-    Q_UNUSED(parentWidget);
+    return QLatin1String(".meocloud");
+}
+
+bool DolphinMEOCloudPlugin::beginRetrieval(const QString& directory)
+{
+    Q_ASSERT(directory.endsWith(QLatin1Char('/')));
+
+    QDBusMessage m = QDBusMessage::createMethodCall("pt.meocloud.dbus",
+                                                    "/pt/meocloud/dbus",
+                                                    "",
+                                                    "GetCloudHome");
+    QDBusMessage response = QDBusConnection::sessionBus().call(m);
+
+    if (response.type() == QDBusMessage::ReplyMessage) {
+        QString cloud_home = response.arguments().at(0).value<QString>();
+        if (!directory.startsWith(cloud_home))
+            return true;
+    } else {
+        return true;
+    }
+
+    m_versionInfoHash.clear();
+
+    QDir dir(directory);
+    QStringList files = dir.entryList();
+
+    for(int i=2;i<files.size();++i)
+    {
+        QString filename = dir.absolutePath() + QDir::separator() + files.at(i);
+
+        KVersionControlPlugin::VersionState versionstate = KVersionControlPlugin::UnversionedVersion;
+
+
+        QDBusMessage m = QDBusMessage::createMethodCall("pt.meocloud.dbus",
+                                                        "/pt/meocloud/dbus",
+                                                        "",
+                                                        "FileInCloud");
+        m << filename;
+        QDBusMessage response = QDBusConnection::sessionBus().call(m);
+
+        if (response.type() == QDBusMessage::ReplyMessage) {
+            bool in_cloud = response.arguments().at(0).value<bool>();
+            bool is_syncing = response.arguments().at(1).value<bool>();
+
+            if (in_cloud && is_syncing) {
+                versionstate = KVersionControlPlugin::UpdateRequiredVersion;
+                m_versionInfoHash.insert(filename, versionstate);
+            } else if (in_cloud) {
+                versionstate = KVersionControlPlugin::NormalVersion;
+                m_versionInfoHash.insert(filename, versionstate);
+            }
+        }
+    }
+
+    return true;
+}
+
+void DolphinMEOCloudPlugin::endRetrieval()
+{
+}
+
+KVersionControlPlugin::VersionState DolphinMEOCloudPlugin::versionState(const KFileItem& item)
+{
+    const QString itemUrl = item.localPath();
+
+    if (m_versionInfoHash.contains(itemUrl))
+    {
+        return m_versionInfoHash.value(itemUrl);
+    }
+
+    return KVersionControlPlugin::UnversionedVersion;
+}
+
+QList<QAction*> DolphinMEOCloudPlugin::contextMenuActions(const KFileItemList& items)
+{
+    Q_ASSERT(!items.isEmpty());
+
+    if(items.size() > 1 || items.size() == 0)
+    {
+        QList<QAction*> emptyactions;
+        m_contextUrl = QString();
+        return emptyactions;
+    }
+
+    KFileItem item = items.at(0);
+    return getActions(item.url().path(), item.isDir());
+}
+
+QList<QAction*> DolphinMEOCloudPlugin::contextMenuActions(const QString& directory)
+{
+    return getActions(directory, true);
+}
+
+QList<QAction*> DolphinMEOCloudPlugin::getActions(QString path, bool is_dir)
+{
     QList<QAction*> actions;
-
-    if (fileItemInfos.items().count() != 1)
-        return actions;
-
-    KFileItem item = fileItemInfos.items().first();
-    QString itemPath = item.localPath();
-    QFileInfo itemFileInfo = QFileInfo(itemPath);
 
     QDBusMessage m = QDBusMessage::createMethodCall("pt.meocloud.dbus",
                                                     "/pt/meocloud/dbus",
                                                     "",
                                                     "FileInCloud");
-    m << itemPath;
+    m << path;
     QDBusMessage response = QDBusConnection::sessionBus().call(m);
 
     if (response.type() == QDBusMessage::ReplyMessage) {
@@ -68,51 +163,35 @@ QList<QAction*> DolphinMEOCloudPlugin::actions(const KFileItemListProperties & f
         return actions;
     }
 
+    m_contextUrl = path;
+
     KActionMenu * menuAction = new KActionMenu(this);
     menuAction->setText("MEO Cloud");
-    actions << menuAction;
 
-    p->contextFilePath = itemPath;
+    menuAction->addAction(m_openInBrowserAction);
 
-    QAction * openInBrowserAction = new KAction(this);
-    openInBrowserAction->setIcon(KIcon("download-later"));
-    openInBrowserAction->setText(i18nc("@item:inmenu", "Open in Browser"));
-    menuAction->addAction(openInBrowserAction);
-    connect(openInBrowserAction, SIGNAL(triggered()),
-            this, SLOT(openInBrowserAction()));
+    if (is_dir)
+        menuAction->addAction(m_shareFolderAction);
+    else
+        menuAction->addAction(m_shareFileLinkAction);
 
-    if (item.isDir()) {
-        QAction * shareFolderAction = new KAction(this);
-        shareFolderAction->setIcon(KIcon("download-later"));
-        shareFolderAction->setText(i18nc("@item:inmenu", "Share Folder"));
-        menuAction->addAction(shareFolderAction);
-        connect(shareFolderAction, SIGNAL(triggered()),
-                this, SLOT(shareFolderAction()));
-    } else {
-        QAction * shareLinkAction = new KAction(this);
-        shareLinkAction->setIcon(KIcon("download-later"));
-        shareLinkAction->setText(i18nc("@item:inmenu", "Copy Link"));
-        menuAction->addAction(shareLinkAction);
-        connect(shareLinkAction, SIGNAL(triggered()),
-                this, SLOT(shareFileLinkAction()));
-    }
-
+    actions.append(menuAction);
     return actions;
 }
 
 void DolphinMEOCloudPlugin::shareFolderAction()
 {
-    this->requestShare(p->contextFilePath);
+    this->requestShare(m_contextUrl);
 }
 
 void DolphinMEOCloudPlugin::openInBrowserAction()
 {
-    this->requestOpen(p->contextFilePath);
+    this->requestOpen(m_contextUrl);
 }
 
 void DolphinMEOCloudPlugin::shareFileLinkAction()
 {
-    this->requestLink(p->contextFilePath);
+    this->requestLink(m_contextUrl);
 }
 
 void DolphinMEOCloudPlugin::requestLink(QString path)
--- meocloud_dolphin/dolphin-meocloud-plugin.h
+++ meocloud_dolphin/dolphin-meocloud-plugin.h
@@ -1,6 +1,7 @@
 #ifndef DolphinMEOCloudPlugin_H
 #define DolphinMEOCloudPlugin_H
 
+#include <kversioncontrolplugin.h>
 #include <kabstractfileitemactionplugin.h>
 #include <QDateTime>
 #include <QHash>
@@ -8,14 +9,19 @@
 #include <QFileInfo>
 #include <QTimer>
 
-class DolphinMEOCloudPlugin : public KAbstractFileItemActionPlugin
+class DolphinMEOCloudPlugin : public KVersionControlPlugin
 {
     Q_OBJECT
 
 public:
     DolphinMEOCloudPlugin(QObject* parent = 0, const QVariantList & args = QVariantList());
     virtual ~DolphinMEOCloudPlugin();
-    virtual QList<QAction*> actions(const KFileItemListProperties & fileItemInfos, QWidget * parentWidget);
+    virtual QString fileName() const;
+    virtual bool beginRetrieval(const QString& directory);
+    virtual void endRetrieval();
+    virtual KVersionControlPlugin::VersionState versionState(const KFileItem& item);
+    virtual QList<QAction*> contextMenuActions(const KFileItemList& items);
+    virtual QList<QAction*> contextMenuActions(const QString& directory);
 
 private slots:
     void shareFolderAction();
@@ -23,11 +29,17 @@ private slots:
     void shareFileLinkAction();
 
 private:
-    struct Private;
-    Private * p;
+    QList<QAction*> getActions(QString url, bool is_dir);
+
+    QAction* m_shareFolderAction;
+    QAction* m_openInBrowserAction;
+    QAction* m_shareFileLinkAction;
+
     void requestLink(QString path);
     void requestShare(QString path);
     void requestOpen(QString path);
 
+    QString m_contextUrl;
+    QHash<QString, KVersionControlPlugin::VersionState> m_versionInfoHash;
 };
 #endif // DolphinMEOCloudPlugin_H
