03bfa70d88e3e73b5ef3bac8a97d482f8e56894f
Ivo Nunes <ivo-a-nunes@telecom.pt>
2014-04-17 10:55:38 +0100

    fixed a bug with the new translation script

--- po/generate_po.sh
+++ po/generate_po.sh
@@ -4,5 +4,5 @@ find .. -iname "*.py" | xargs xgettext
 sed -i 's/CHARSET/UTF-8/g' messages.po
 
 for lang in pt; do
-    msgmerge ${lang}.po messages.po > ${lang}.po
+    msgmerge -U ${lang}.po messages.po
 done
