4a5d7272527b228ceba576815f2b4c0a3a1c23ce
Ivo Nunes <ivo-a-nunes@telecom.pt>
2014-04-29 10:49:07 +0100

    let advanced users select their initial MEO Cloud folder

--- meocloud_gui/core/core_listener.py
+++ meocloud_gui/core/core_listener.py
@@ -95,10 +95,36 @@ class CoreListenerHandler(UI.Iface):
         self.setup.show_all()
 
     def beginAuthorizationBrowser(self, w):
-        try:
-            meocloud_gui.utils.clean_cloud_path()
-        except (OSError, IOError, AssertionError):
-            return
+        dialog = Gtk.MessageDialog(
+            None, 0, Gtk.MessageType.QUESTION,
+            Gtk.ButtonsType.YES_NO,
+            _("Would you like to select a different MEO Cloud folder?"))
+        response = dialog.run()
+        dialog.destroy()
+
+        if response == Gtk.ResponseType.YES:
+            dialog = Gtk.FileChooserDialog(
+                _("Please choose a folder"), self.setup,
+                Gtk.FileChooserAction.SELECT_FOLDER,
+                (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                    _("Select"), Gtk.ResponseType.OK))
+            dialog.set_default_size(800, 400)
+            response = dialog.run()
+
+            if response == Gtk.ResponseType.OK:
+                cloud_home = os.path.join(dialog.get_filename())
+                dialog.destroy()
+                print cloud_home
+                self.ui_config.put('Advanced', 'Folder', cloud_home)
+                print "set"
+            else:
+                dialog.destroy()
+                return
+        else:
+            try:
+                meocloud_gui.utils.clean_cloud_path()
+            except (OSError, IOError, AssertionError):
+                return
 
         self.setup.start_waiting()
 
