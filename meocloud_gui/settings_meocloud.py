a934671707f74ffa4d1e57a5133d00b08a5b2777
sofia cardita <sofia-b-cardita@telecom.pt>
2015-02-03 12:28:53 +0000

    Protocol should be https.

--- meocloud_gui/settings_meocloud.py
+++ meocloud_gui/settings_meocloud.py
@@ -3,5 +3,5 @@ BRAND = "meocloud"
 BASE_PATH = "/opt/{0}".format(BRAND)
 BRAND_FOLDER_NAME = "MEOCloud"
 BRAND_PROGRAM_NAME = "MEO Cloud"
-BRAND_WEBSITE = "http://www.meocloud.pt"
+BRAND_WEBSITE = "https://www.meocloud.pt"
 BRAND_HELP_WEBSITE = "https://ajuda.cld.pt"
--- meocloud_gui/settings_oicloud.py
+++ meocloud_gui/settings_oicloud.py
@@ -3,5 +3,5 @@ BRAND = "oicloud"
 BASE_PATH = "/opt/{0}".format(BRAND) # TODO maybe remove
 BRAND_FOLDER_NAME = "OICloud"
 BRAND_PROGRAM_NAME = "OI Cloud"
-BRAND_WEBSITE = "http://www.oicloud.com.br"
+BRAND_WEBSITE = "https://www.oicloud.com.br"
 BRAND_HELP_WEBSITE = "https://oicloud.com.br/ajuda"
