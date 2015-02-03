98c73d8626f2b198de7f9c9ec9d9b6b4aabd8850
sofia cardita <sofia-b-cardita@telecom.pt>
2015-02-03 12:29:48 +0000

    Remove deprecated settings.

--- meocloud_gui/settings_meocloud.py
+++ meocloud_gui/settings_meocloud.py
@@ -1,6 +1,5 @@
 
 BRAND = "meocloud"
-BASE_PATH = "/opt/{0}".format(BRAND)
 BRAND_FOLDER_NAME = "MEOCloud"
 BRAND_PROGRAM_NAME = "MEO Cloud"
 BRAND_WEBSITE = "https://www.meocloud.pt"
--- meocloud_gui/settings_oicloud.py
+++ meocloud_gui/settings_oicloud.py
@@ -1,6 +1,5 @@
 
 BRAND = "oicloud"
-BASE_PATH = "/opt/{0}".format(BRAND) # TODO maybe remove
 BRAND_FOLDER_NAME = "OICloud"
 BRAND_PROGRAM_NAME = "OI Cloud"
 BRAND_WEBSITE = "https://www.oicloud.com.br"
